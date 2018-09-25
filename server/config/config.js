e8983be47ae47bd8d78dad3c2c4960dd3a87d0df
AtilaDev <atiladevelop@gmail.com>
2018-09-25 12:12:06 -0300

    Variable de entorno MONGO

--- server/config/config.js
+++ server/config/config.js
@@ -16,8 +16,7 @@ let urlDB;
 if (process.env.NODE_ENV === 'dev') {
   urlDB = 'mongodb://localhost:27017/cafe';
 } else {
-  urlDB =
-    'mongodb://<user>:<password>@ds113703.mlab.com:13703/cafe';
+  urlDB = process.env.MONGO_URI;
 }
 
 process.env.URLDB = urlDB;
