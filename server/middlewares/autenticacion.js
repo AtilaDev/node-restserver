6cf78a6eefb1980cd9087006e7853d5c01cb38da
AtilaDev <atiladevelop@gmail.com>
2018-10-08 12:20:53 -0300

    Login normal seccion 10

---
+++ server/middlewares/autenticacion.js
@@ -0,0 +1,44 @@
+const jwt = require('jsonwebtoken');
+
+// =========================
+//  Verificacion de Token
+// =========================
+let verificaToken = (req, res, next) => {
+  let token = req.get('token');
+
+  jwt.verify(token, process.env.SEED, (err, decoded) => {
+    if (err) {
+      return res.status(401).json({
+        ok: false,
+        message: 'Token no válido',
+        err
+      });
+    }
+
+    req.usuario = decoded.usuario;
+    next();
+  });
+};
+
+// =========================
+//  Verifica AdmiRole
+// =========================
+let verificaAdmin_Role = (req, res, next) => {
+  let usuario = req.usuario;
+
+  if (usuario.role === 'ADMIN_ROLE') {
+    next();
+  } else {
+    return res.json({
+      ok: false,
+      err: {
+        message: 'El usuario no es administrador'
+      }
+    });
+  }
+};
+
+module.exports = {
+  verificaToken,
+  verificaAdmin_Role
+};
